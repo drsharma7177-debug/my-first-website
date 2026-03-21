6686af367613da4b56aefad156d97fc502463317
drsharma7177-debug <drsharma7177@gmail.com>
2026-03-21 10:27:04 +0000

    Add files via upload
    website added

---
+++ html index.jsx
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+      <title> My First Website</title>
+        </head>
+
+         <body>
+       <h1>Hello</h1>
+     <p>I am Learning HTML</p>
+            </body>
+
+            </html>
+git init 
+git add.
+git commit -m "message"
+git branch -M main 
+git remote add origin https://github.com/DUKARAJ/my-first-project.git
+git push -u origin main 
